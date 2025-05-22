b2c5cd629d215ee0d149b374a6524c0b606d63a0
MC0D <mik.fdc@hotmail.fr>
2025-05-22 17:46:30 +0200

    création à la volée des cartes évènements, infos affichés avec les boutons

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/* RECUPERATION DES ID POUR LES SECTIONS */
+
+const body = document.body;
+const events = document.getElementById("events");
+const planning = document.getElementById("planning");
+
+/* RECUPERATION DES ID POUR LA MODALE */
+
+const divModal = document.getElementById("modal");
+const buttonCloseModal = document.getElementById("close-modal");
+const titreModal = document.getElementById("modal-title");
+const descriptionModal = document.getElementById("modal-description");
+const dateModal = document.getElementById("modal-date");
+const venueModal = document.getElementById("modal-venue");
+const linkModal = document.getElementById("modal-link");
+
+
+/* RECUPERATION DES DONNEES VIA L'API */
+
 async function fetchData() {
   try {
     const response = await fetch(
@@ -14,9 +33,40 @@ async function fetchData() {
       console.log(event.description);
       console.log(event.date);
       console.log(event.venue.city);
+      /* CREATION A LA VOLEE DES CARTES EVENEMENTS */
+      /* STRUCTURE */
+      const div = document.createElement("div");
+      const title = document.createElement("h4");
+      const date = document.createElement("p");
+      const lieu = document.createElement("p")
+      const description = document.createElement("p");
+      const buttonDetails = document.createElement("button");
+      const buttonAdd = document.createElement("button");
+      /* CONTENU DES EVENEMENTS ET BOUTONS */
+      title.textContent = event.title;
+      date.textContent = event.date;
+      description.textContent = event.description;
+      buttonDetails.textContent = "Voir détails";
+      buttonAdd.textContent = "Ajouter";
+      if (event.venue != "" && event.venue != null)  {
+        lieu.textContent = `${event.venue.address} / ${event.venue.city}
+      / ${event.venue.venue}`;
+      } else {
+        lieu.textContent = "Pas d'adresse disponible"
+      }
+      /* AJOUT A LA SECTION */
+      div.appendChild(title);
+      div.appendChild(date);
+      div.appendChild(lieu);
+      div.appendChild(description);
+      div.appendChild(buttonDetails);
+      div.appendChild(buttonAdd);
+      events.appendChild(div);
     });
   } catch {
     console.log("erreur lors de la récupérations des données");
   }
 }
 fetchData();
+
+
